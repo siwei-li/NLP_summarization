07c144b6d2bbd334862759ff3dfa0fe236edf503
sil <46750682+siwei-li@users.noreply.github.com>
2021-11-05 14:39:08 -0700

    Add raw data(in files)

--- preprocess/process.py
+++ preprocess/process.py
@@ -7,25 +7,30 @@ Each summary line starts with tag "@summary" and the article is followed by "@ar
 import pandas as pd
 import os
 import re
+import sys
 
 # read data from the csv file (from the location it is stored)
-Data = pd.read_csv(r'cleaned_transcripts.csv')
+# Data = pd.read_csv(r'cleaned_transcripts.csv')
+Data = pd.read_csv(os.path.join(sys.path[0],'TED_Talk.csv'))
+
 Data = Data.astype(str)
 rows, columns = Data.shape
 
 # create a file to record the file names. This can be later used to divide the dataset in train/dev/test sets
-title_file = open('titles.txt', 'wb')
+title_file = open(os.path.join(sys.path[0], 'titles.txt'), 'wb')
 
 # The path where the articles are to be saved
-path = "articles"
+path = os.path.join(sys.path[0], "articles")
 if not os.path.exists(path): os.makedirs(path)
 
 # go over the all the articles in the data file
 for row in range(rows):
-    abstract = Data.loc[row,'summary']      # headline is the column representing the summary sentences # Bryan changed from ix to loc
+    abstract = Data.loc[row,'talk__description']
+    # abstract = Data.loc[row,'summary']      # headline is the column representing the summary sentences # Bryan changed from ix to loc
     article = Data.loc[row,'transcript']           # text is the column representing the article
 
     #  a threshold is used to remove short articles with long summaries as well as articles with no summary
+    # if len(article) > 200:
     if len(article) > 20:
 
         # remove extra commas in abstracts
@@ -40,12 +45,14 @@ for row in range(rows):
 
 
         # a temporary file is created to initially write the summary, it is later used to separate the sentences of the summary
-        with open('temporaryFile.txt','wb') as t:
+        with open(path+'/'+'temporaryFile.txt','wb') as t:
             t.write(abstract)
 
         # file names are created using the alphanumeric charachters from the article titles.
         # they are stored in a separate text file.
-        filename = Data.loc[row,'title']
+        # filename = Data.loc[row,'title']
+        filename = Data.loc[row,'talk__name']
+
         filename = "".join(x for x in filename if x.isalnum())
         filename1 = filename + '.txt'
         filename = filename.encode('utf-8')
@@ -54,7 +61,7 @@ for row in range(rows):
 
         with open(path+'/'+filename1,'wb') as f:
             # summary sentences will first be written into the file in separate lines
-            with open('temporaryFile.txt','r', encoding="utf8") as t: # Bryan added encoding
+            with open(path+'/'+'temporaryFile.txt','r', encoding="utf8") as t: # Bryan added encoding
                 for line in t:
                     line=line.lower()
                     if line != "\n" and line != "\t" and line != " ":
